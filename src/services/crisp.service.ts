2815a4008a93434c1546fabb9a6421527a628d9d
HungPH <hungph.tc23@gmail.com>
2025-06-16 16:04:58 +0700

    fix build project

--- src/services/crisp.service.ts
+++ src/services/crisp.service.ts
@@ -1,13 +1,11 @@
 import { ConversationResponse } from '@/modules/crisp/crisp.module';
 import BaseAPI from './base.service';
-import CrispMock from './mockdata/crisp.mock';
 
 const PATH_URL = `api/crisp`;
 
 // Get all conversation
 const getAllConversationRequest = async () => {
     return BaseAPI.GET<ConversationResponse[]>(`${PATH_URL}/conversation`);
-    // return CrispMock.Conversation
 }
 
 const CrispService = {
